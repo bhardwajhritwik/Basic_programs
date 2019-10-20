05f90d4ed0dcbab7b305b124e1a8734b490a71a0
Hritwik Bhardwaj <46474138+bhardwajhritwik@users.noreply.github.com>
2019-10-20 16:47:56 +0530

    Update LcmGcd.py

--- Python Programs/LcmGcd.py
+++ Python Programs/LcmGcd.py
@@ -1,5 +1,9 @@
 #Program to find GCD and LCM of given numbers
 from math import gcd
+
+def lc(a,b):
+    return ((a*b)/gcd(a,b))
+
 for x in range(int(input())):
     (a,b)=[int(m) for m in input().split()]
     k=gcd(a,b)
